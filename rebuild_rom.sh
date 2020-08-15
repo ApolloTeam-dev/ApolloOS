38a06cd7c5b0bab0ce0dd4b5a8b80a8524928954
Ronnie Beck <aaron.scott@themenz.biz>
2020-08-15 20:41:18 +0200

    Removed ARM arch

--- rebuild_rom.sh
+++ rebuild_rom.sh
@@ -4,7 +4,7 @@ git clean -df
 rm -rf bin/amiga-m68k
 ./configure --target=amiga-m68k --with-optimization="-Os" --enable-ccache --with-aros-prefs=classic --with-resolution=640x256x4 --with-cpu=68020
 make -j12
-make -j12 distfiles
+make -j12 kernel-amiga-m68k
 cat bin/amiga-m68k/gen/boot/aros-amiga-m68k-ext.bin bin/amiga-m68k/gen/boot/aros-amiga-m68k-rom.bin > aros.rom
 ls -lah aros.rom
 
