bf0fbcc4aac61aed3b151fe56b4735414a04613e
Ronnie Beck <aaron.scott@themenz.biz>
2020-12-15 20:52:00 +0100

    Changed name in early start menu to ApollOS

--- rom/dosboot/menu.c
+++ rom/dosboot/menu.c
@@ -460,7 +460,7 @@ static void initPage(LIBBASETYPEPTR DOSBootBase, WORD page)
     else if (page == PAGE_BOOT)
         text = "Boot Options";
     else
-        text = "ApolloOS Early Startup Control";
+        text = "ApollOS Early Startup Control";
     centertext(DOSBootBase, 2, 10, text);
     
     if (page == PAGE_BOOT)
@@ -480,7 +480,7 @@ static void initPage(LIBBASETYPEPTR DOSBootBase, WORD page)
     if (page == PAGE_MAIN && (GfxBase->DisplayFlags & (NTSC | PAL))) {
             ULONG modeid = GetVPModeID(&DOSBootBase->bm_Screen->ViewPort);
             if (modeid != INVALID_ID && (((modeid & MONITOR_ID_MASK) == NTSC_MONITOR_ID) || ((modeid & MONITOR_ID_MASK) == PAL_MONITOR_ID))) {
-            centertext(DOSBootBase, 1, 30, "(Release 2, 2020-08-09)");
+            centertext(DOSBootBase, 1, 30, "(Release 5, 2020-12-12)");
         }
     }
 
