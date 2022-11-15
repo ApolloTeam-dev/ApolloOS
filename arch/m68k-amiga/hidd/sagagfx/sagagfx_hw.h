0c1e9083f10294a7b9e59e9fac71f46ed53dc701
Willem Drijver <60568860+WDrijver@users.noreply.github.com>
2022-11-15 10:43:52 +0100

    Update sagagfx_hw.h
    Updated VREG_BOARD define to match current line-up.

--- arch/m68k-amiga/hidd/sagagfx/sagagfx_hw.h
+++ arch/m68k-amiga/hidd/sagagfx/sagagfx_hw.h
@@ -22,15 +22,15 @@
 */
 
 #define VREG_BOARD_Unknown      0x00      // Unknown
-#define VREG_BOARD_V600         0x01      // V600
-#define VREG_BOARD_V500         0x02      // V500
-#define VREG_BOARD_V4           0x03      // V4-500
-#define VREG_BOARD_ICEDRAKE     0x04      // V4-Icedrake
-#define VREG_BOARD_V4SA         0x05      // V4SA
-#define VREG_BOARD_V1200        0x06      // V1200
-#define VREG_BOARD_V4000        0x07      // V4000
-#define VREG_BOARD_VCD32        0x08      // VCD32
-#define VREG_BOARD_Future       0x09      // Future
+#define VREG_BOARD_V2_V600      0x01      // V2-V600 (legacy)
+#define VREG_BOARD_V2_V500      0x02      // V2-V500 (legacy)
+#define VREG_BOARD_V4_V500      0x03      // V4-V500 = FireBird
+#define VREG_BOARD_V4_V1200     0x04      // V4-V1200 = Icedrake
+#define VREG_BOARD_V4_SA        0x05      // V4-SA = StandAlone
+#define VREG_BOARD_V2_V1200     0x06      // V2-V1200 (legacy)
+#define VREG_BOARD_V4_V600      0x07      // V4-V600 = MantiCore
+#define VREG_BOARD_Future_1     0x08      // Future
+#define VREG_BOARD_Future_2     0x09      // Future
 
 #define VREG_BOARD              0xDFF3FC  // [16-bits] BoardID [HIGH-Byte: MODEL, LOW-Byte: xFREQ]
 
