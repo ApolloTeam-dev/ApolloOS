171696b4488c6ce66312646ee3b0979c0f31cd7c
WDrijver <willemdrijver@mac.com>
2025-03-21 10:18:20 +0100

    gui.h (BHFormat)

--- workbench/system/BHFormat/gui.c
+++ workbench/system/BHFormat/gui.c
@@ -76,26 +76,7 @@
 */
 
 #define MUIMASTER_YES_INLINE_STDARG
-#include <libraries/mui.h>
-#include <proto/muimaster.h>
 
-#include <dos/dosextens.h>
-#include <dos/filehandler.h>
-#include <intuition/intuition.h>
-#include <stdarg.h>
-#include <workbench/startup.h>
-
-#include <proto/alib.h>
-#include <proto/dos.h>
-#include <proto/exec.h>
-#include <proto/gadtools.h>
-#include <proto/graphics.h>
-#include <proto/intuition.h>
-
-#include <string.h>
-
-#include "format.h"
-#include "locale.h"
 
 static char szCapacityInfo[5+1+8+2+2+2+4+1];
 static Object *app, *mainwin, *formatwin, *chk_trash, *chk_intl, *chk_ffs, *chk_cache;
