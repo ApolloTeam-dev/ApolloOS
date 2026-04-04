ef2849975c49b4739f0d33e48b636e5a73c8789c
Jens Nyberg <jens.nyberg@gmail.com>
2026-04-04 15:20:01 +0200

    Update docker build
    This commit rewrites the ApolloOS build image.
    
    If you have a machine with docker installed, running ./docker.sh is now all
    you need to build ApolloOS.
    
    Dockerfile:
    
    * New base image is ubuntu 22.04
    * Dependencies changed to match dependencies.sh
    * Removed PS1 manipulations to clean things up
    
    docker.sh:
    
    * Changed tag name to apollo-os-build:latest
    * Changed the check if the image already was built.
    * Container is now run as your own user instead of root.
    * Now uses ./mkapollo.sh all to build.
    
    mkapollo.sh:
    
    * Add flag to override ports. This was necesserry in my case because the
      downloads did not work and since the source was already part of the
      git source I just pointed it to that directory instead.

--- docker.sh
+++ docker.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-#sudo docker run -it --rm -v "$PWD":/git ubuntu "/bin/bash -c \"apt-get install git-core gcc g++ make cmake gawk bison flex bzip2 netpbm autoconf automake libx11-dev libxext-dev libc6-dev liblzo2-dev libxxf86vm-dev libpng-dev libsdl1.2-dev byacc python-mako libxcursor-dev gcc-multilib\"; /bin/bash"
-IMAGE_NAME=apolloos/builder
 
+IMAGE_NAME=apollo-os-build:latest
 
-#Do we have this image already?
-DOCKER_IMAGE_COUNT=$(docker image ls | awk -F\  '{ print $1 }' | grep "${IMAGE_NAME} | wc -l")
-echo ${DOCKER_IMAGE_COUNT}
-if [ "${DOCKER_IMAGE_COUNT}" == "" ]
+if [ -z "$(docker images -q "${IMAGE_NAME}")" ]
 then
-	docker build -t ${IMAGE_NAME} .
+    docker build -t "${IMAGE_NAME}" .
 fi
 
-#Run the docker and start the build
-docker run -it --rm -v "$PWD":/usr/src ${IMAGE_NAME} /bin/bash -c "cd /usr/src; ./rebuild_all.sh"
+docker run -it --rm -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v "${PWD}":/usr/src -u "$(id -u):$(id -g)" "${IMAGE_NAME}" /bin/bash -c "./mkapollo.sh --ports=/usr/src/bin/Sources all"
