5ecab1645d228d9aefb4a89091bb91d10843e9c0
Willem Drijver <willemdrijver@mac.com>
2026-05-11 08:28:48 +0200

    Updated dependencies scripts

---
+++ dependencies_macos.sh
@@ -0,0 +1,2 @@
+brew install git gcc make cmake gawk bison flex bzip2 netpbm autoconf automake libx11 libxext libpng byacc libxcursor
+brew install netpbm cdrtools gawk py-pil wget gsed cmake autoconf automake
